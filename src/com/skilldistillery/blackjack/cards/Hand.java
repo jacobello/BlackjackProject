1023b5fbfe1dd63ce5008bd85daad039361ac916
Jacob Nguyen <jn@jacobello.com>
2022-04-10 21:49:38 -0600

    add all classes

--- src/com/skilldistillery/blackjack/cards/Hand.java
+++ src/com/skilldistillery/blackjack/cards/Hand.java
@@ -1,5 +1,44 @@
 package com.skilldistillery.blackjack.cards;
 
-public abstract class Hand {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Hand {
+	protected List<Card> cards;
+
+	public Hand() {
+		cards = new ArrayList<Card>();
+	}
+
+	public Hand(List<Card> cards) {
+		super();
+		this.cards = cards;
+	}
+
+	public abstract boolean isBust();
+
+	public abstract boolean isBlackJack();
+
+	public abstract boolean isTwentyOne();
+
+	public abstract int getHandValue();
+
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+
+	public void clear() {
+		cards.clear();
+	}
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Hand: ").append(cards).append(".");
+		return builder.toString();
+	}
 }
